5c94286f5a46b919b092f474f94cb7a46db4b7c7
adam.santos <adam.santos@objective.com.br>
2023-04-11 17:52:46 -0300

    fix: transcription to work and requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import base64
-import io
 import json
 import logging
 import os
-
-import audioop
-from pydub import AudioSegment
 
 from flask import Flask
 from flask_sockets import Sockets
@@ -14,8 +10,10 @@ import azure.cognitiveservices.speech as speechsdk
 AZURE_SPEECH_SUBSCRIPTION_KEY = os.environ.get("AZURE_SPEECH_SUBSCRIPTION_KEY")
 AZURE_SERVICE_REGION = os.environ.get("AZURE_SERVICE_REGION")
 
-speech_config = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_SUBSCRIPTION_KEY, region=AZURE_SERVICE_REGION, speech_recognition_language='en-US')
-wave_format = speechsdk.audio.AudioStreamFormat(samples_per_second=16000, bits_per_sample=16, channels=1)
+speech_config = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_SUBSCRIPTION_KEY, region=AZURE_SERVICE_REGION,
+                                       speech_recognition_language='en-US')
+wave_format = speechsdk.audio.AudioStreamFormat(samples_per_second=8000, bits_per_sample=16, channels=1,
+                                                wave_stream_format=speechsdk.AudioStreamWaveFormat.MULAW)
 audio_stream = speechsdk.audio.PushAudioInputStream(stream_format=wave_format)
 audio_config = speechsdk.audio.AudioConfig(stream=audio_stream)
 
@@ -28,23 +26,18 @@ speech_recognizer.canceled.connect(lambda evt: on_canceled(evt))
 
 
 def on_canceled(evt):
+    speech_recognizer.stop_continuous_recognition()
     print('CANCELED')
     print(evt)
     print(evt.result)
     print(evt.result.cancellation_details)
+
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
 HTTP_SERVER_PORT = 5000
 
-
-def on_transcription_response(result):
-    if not result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        return
-
-    transcription = result.text
-    print("Transcription: " + transcription)
 
 @sockets.route('/media')
 def echo(ws):
@@ -69,18 +62,12 @@ def echo(ws):
             app.logger.info("Start Message received: {}".format(message))
         if data['event'] == "media":
             if not has_seen_media:
-                # app.logger.info("Media message: {}".format(message))
                 payload = data['media']['payload']
-                # app.logger.info("Payload is: {}".format(payload))
                 chunk = base64.b64decode(payload)
-                # app.logger.info("That's {} bytes".format(len(chunk)))
-                # app.logger.info("Additional media messages from WebSocket are being suppressed....")
-                wav_data = AudioSegment.from_file(io.BytesIO(chunk), format='mulaw').set_frame_rate(16000).set_sample_width(2).set_channels(1).raw_data
-                audio_stream.write(wav_data)
-                # has_seen_media = True
+                audio_stream.write(chunk)
         if data['event'] == "stop":
-            app.logger.info("Stop Message received: {}".format(message))
             speech_recognizer.stop_continuous_recognition()
+            app.logger.info("Stop Message received: {}".format(message))
             break
         message_count += 1
 
